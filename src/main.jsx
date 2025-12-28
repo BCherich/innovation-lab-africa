4e85300861140069679fa5aed3899ba16f5e065f
Kones Cherich <73218601+BCherich@users.noreply.github.com>
2025-12-28 11:16:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import InnovationLabAfrica from './InnovationLabAfrica';
 import AdminCMS from './AdminCMS';
 
 const App = () => {
   const isAdminRoute = window.location.pathname.startsWith('/admin');
-  
+
   if (isAdminRoute) {
     return <AdminCMS />;
   }
-  
+
   return <InnovationLabAfrica />;
 };
+
+// Render the App into the DOM
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  // Helpful fallback / debug message in case index.html doesn't have the root element
+  console.error('Root element not found: make sure your index.html contains <div id="root"></div>');
+} else {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 export default App;
